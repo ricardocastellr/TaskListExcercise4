7c6c99f85e71c24173e39da975d8b3e28cbca476
ricardocastell <rcastell@ucol.mx>
2024-03-15 12:09:22 -0600

    Add total of completed tasks

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,14 +2,22 @@ import { Task } from '../Task/Task';
 import './TaskList.css';
 
 export function TaskList({ tasks, onTaskCompletion }) {
+  // Contador para almacenar el número de tareas completadas
+  const completedCount = tasks.filter(task => task.completed).length;
+
   return (
-    tasks.map(task => (
-      <Task
-        key={task.id}
-        title={task.title}
-        completed={task.completed}
-        onCompletion={() => onTaskCompletion(task.id)}
-      />
-    ))
+    <div>
+      {/* Renderiza cada tarea con su respectivo estado */}
+      {tasks.map(task => (
+        <Task
+          key={task.id}
+          title={task.title}
+          completed={task.completed}
+          onCompletion={() => onTaskCompletion(task.id)}
+        />
+      ))}
+      {/* Muestra el contador de tareas completadas */}
+      <p>Total de tareas completadas: {completedCount}</p>
+    </div>
   );
 }
